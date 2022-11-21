069e5dc0020d8ba24f89220d16d6715e20f06d94
Sergio <sertrujez0420@gmail.com>
2022-11-21 20:06:49 +0100

    subir

---
+++ 02_EjerciciosT2/1Ejemplos clase/multiplicar.java
@@ -0,0 +1,15 @@
+
+public class multiplicar {
+
+    public static void main(String[] args) {
+        System.out.println("Argumento 1 " + args[0]);
+        System.out.println("Argumento 2 " + args[1]);
+
+        int n1 = Integer.parseInt(args[0]);
+        int n2 = Integer.parseInt(args[1]);
+
+        System.out.println("Resultado de la multiplicacion " + (n1 * n2));
+        
+        
+    }
+}
