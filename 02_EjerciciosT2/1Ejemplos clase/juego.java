67b3735a04b5fb3e8a945ece291e0db41e316f3b
Sergio <sertrujez0420@gmail.com>
2022-11-21 21:25:05 +0100

    subir

---
+++ 02_EjerciciosT2/1Ejemplos clase/juego.java
@@ -0,0 +1,19 @@
+
+public class juego {
+    public static void main(String[] args) {
+        
+        // creamos una lista que contenga todas las palabras reservadas de java 
+        String[] list = {"boolean", "byte", "char", "short", "int",
+        "long", "float", "double", "void", "false", "true", "null",
+        "abstract", "final", "native", "private", "protected", "public",
+        "static", "synchronized", "transieent", "volatile", "break", "case",
+        "catch", "continue", "default", "do", "else", "finally", "for", "if",
+        "return", "switch", "throw", "try", "while", "class", "extends", "implements",
+        "interface", "throws", "import", "package", "instanceof", "new", "supr", "this",
+        "byvalue", "cast", "const", "future", "generic", "goto", "inner", "operator", "outer",
+        "rest", "var"};
+
+        System.out.println(list.length);
+
+    }
+}
