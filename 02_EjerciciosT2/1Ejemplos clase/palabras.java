082643948e416d9ade35261f8f5145fb5cc92b95
Sergio <sertrujez0420@gmail.com>
2022-11-25 20:25:14 +0100

    avance

--- 02_EjerciciosT2/1Ejemplos clase/palabras.java
+++ 02_EjerciciosT2/1Ejemplos clase/palabras.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class palabras {
     public static void main(String[] args) {
@@ -15,11 +16,76 @@ public class palabras {
 
         // la lista que comprueba cuantos quedan
 
-        boolean[] com = {false, false, false, false, false, false, false, false, false, false, false, 
-            false, false, false, false, false, false, false, false, false, false, false, false, false, 
-            false, false, false, false, false, false, false, false, false, false, false, false, false, 
-            false, false, false, false, false, false, false, false, false, false, false, false, false,
-            false, false, false, false, false, false, false, false, false};
+        String[] com = {"", "", "", "", "", "", "", "", "", "", "", 
+        "", "", "", "", "", "", "", "", "", "", "", "", "", 
+        "", "", "", "", "", "", "", "", "", "", "", "", "", 
+        "", "", "", "", "", "", "", "", "", "", "", "", "",
+        "", "", "", "", "", "", "", "", ""
+        };
+
+        // creamos la variable que cuente el numero de aciertos 
+
+        int a = 0;
+        boolean fin = false;
+
+
+        // creamos un bucle que pregunte al usuario por telcado las palabras, este bucle no se rompera a menos que
+        // // no queden mas palabras reservadas o el usuario presione x 
+
+        while (fin == false) {
+
+            // pedimos que introduza una palabra por telcado 
+
+            System.out.println("Introduce una palabra reservada de java :D ");
+            String pala = new Scanner(System.in).nextLine();
+            
+            // creamos el metodo rompedor del bucle
+
+            switch (pala) {
+
+                // en caso de escribir x cambiamos el valor de la variable que utilizamos para terminar el bucle 
+
+                case "x":
+                    fin = true;
+                    break;
+
+                default:
+
+                    // creamos un bucle que recorra la lista de las palabras reservadas 
+    
+                    
+
+                    for (int i = 0; i < list.length; i++) 
+
+                        // comprobamos que la palabra introducida corresponda con alguna de la lista 
+                
+                        if (pala == list[i]) {
+                            com[i] = pala;
+                            a++; 
+                            System.out.println(a);
+
+                            if (a == list.length){
+                                fin = true;
+                            }
+                        }
+                    }
+            }
+
+            
+        // }
+            
+
+        for (int i = 0; i < com.length; i++) {
+
+            // utilizamos este if para no imprimir los lugares vacios 
+
+            if (com[i] != ""){
+                System.out.println(com[i]);
+            }
+        }
+
+        System.out.println("Has acertardo " + a + "/" + list.length );
+        
 
     
 
