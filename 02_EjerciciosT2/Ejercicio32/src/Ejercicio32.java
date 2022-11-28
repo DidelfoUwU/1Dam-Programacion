30415d7a0ab2de0a4db69d9abb4baf6b8ca5bb2d
Sergio <sertrujez0420@gmail.com>
2022-11-28 20:33:46 +0100

    subir

--- 02_EjerciciosT2/Ejercicio32/src/Ejercicio32.java
+++ 02_EjerciciosT2/Ejercicio32/src/Ejercicio32.java
@@ -8,5 +8,19 @@ public class Ejercicio32 {
         // creamos el constuctor del archivo mp3 
 
         ArchivoMP3 ar = new ArchivoMP3("C:/Users/Sergio/Music/sin ti.mp3");
+        
+        // creamos el reproductor con el constructro 
+        Reproductor re = new Reproductor(ar, true, false);
+       
+        // imprimimos por pantalla los datos que nos piden
+        
+        System.out.println("El titulo de la cancion es: " + ar.getTitulo());
+        System.out.println("El autor es: " + ar.getAutor());
+        System.out.println("La duracion de la cancion es: " + ar.getDuracion());
+        
+        // Reproducimos la cancion 
+        
+        re.play();
+        
     }
 }
