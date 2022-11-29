a2f9d27a0588d9ec42eac71f11ddfe69f608a66a
Sergio <sertrujez0420@gmail.com>
2022-11-29 17:31:02 +0100

    subir

---
+++ 02_EjerciciosT2/Ejercicio45.java
@@ -0,0 +1,13 @@
+import java.time.LocalDate;
+
+public class Ejercicio45 {
+    public static void main(String[] args) {
+
+        LocalDate no = LocalDate.of(2100, 2, 28);
+
+        System.out.println("Sera " + no.getDayOfWeek());
+        
+
+
+    }
+}
