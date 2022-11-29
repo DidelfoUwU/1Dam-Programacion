4ae8dd2c240baf2b2b3e0a06c25d479fabddca32
Sergio <sertrujez0420@gmail.com>
2022-11-29 18:04:17 +0100

    subir

---
+++ 02_EjerciciosT2/Ejercicio47.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Ejercicio47 {
+    public static void main(String[] args) {
+        
+        // le pedimos al usuario la ruta 
+
+        System.out.println("Introduce la ruta");
+        String ru = new Scanner(System.in).nextLine();
+
+        
+        
+
+    }
+}
