d69f6a7fd3331bd24d7a9c2235eb6f75719732d6
DidelfoUwU <sertrujez@gmail.com>
2022-12-01 12:49:17 +0100

    subir

---
+++ 02_EjerciciosT2/Ejercicio36.java
@@ -0,0 +1,27 @@
+public class Ejercicio36 {
+    public static void main(String[] args) {
+        
+        // calculamos la raiz cuadaada 
+
+        System.out.println("La raiz cuardrada de 150 es: " + 
+        Math.sqrt(150));
+
+        // calculamos la potencia entre el logaritmo 
+
+        System.out.println("El resultado de (2^9 / log(6)) es: " + 
+        (Math.pow(2, 9) / Math.log(6)));
+
+        // calculamos el coseno 
+
+        System.out.println("El coseno de 0.16 radianes es: " +
+        Math.cos(0.16));
+
+        // calculamos el seno y lo mostrasmos en radianes 
+
+        System.out.println("El seno de 45 grados son " +
+        Math.sin(Math.toRadians(45)) + " radianes" );
+        
+        
+        
+    }
+}
